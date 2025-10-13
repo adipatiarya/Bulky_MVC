91897cbfbc4d0f3ae6ace65e1024253e73226ecf
Kirun <you@example.com>
2025-10-13 22:24:03 +0700

    Update Category

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -13,6 +13,7 @@ namespace BulkyWeb.Models
         [DisplayName("Category Name")]
         public string Name { get; set; }
 
+        [Required]
         [DisplayName("Display Order")]
         [Range(1,100, ErrorMessage = "The field Display Order must be between 1 -100.")]
         public int DisplayOrder { get; set; }
