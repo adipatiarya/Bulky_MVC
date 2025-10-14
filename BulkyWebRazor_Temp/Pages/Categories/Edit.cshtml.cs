9348460497ec2fdc549014bb55355d12b78364fc
Kirun <you@example.com>
2025-10-14 13:40:08 +0700

    Edit and Delete Category

--- BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -3,38 +3,41 @@ using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-
-
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
-
     [BindProperties]
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
 
-        
-        public Category Category { get; set; } //nama ini di pake di view
 
-        public CreateModel(ApplicationDbContext db)
+        public Category? Category { get; set; } //nama ini di pake di view
+
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
 
         }
 
-        public void OnGet() { }
+        public void OnGet(int? id) {
+            if(id != null && id != 0 )
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
 
         public IActionResult OnPost()
         {
-
-            if (ModelState.IsValid) {
-                _db.Categories.Add(Category);
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
                 _db.SaveChanges();
                 return RedirectToPage("Index");
             }
+
             return Page();
 
+
         }
-  
     }
 }
