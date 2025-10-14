2993e2d88b230d317fd319f2ab5c43f00c396fd9
Kirun <you@example.com>
2025-10-14 22:52:14 +0700

    InternalUnit

---
+++ Bulky.DataAccess/Services/InternalUnit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccess.Services.IServices;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Services
+{
+    public class InternalUnit:IInternal
+    {
+        private ApplicationDbContext _context;
+        public CategoryRepository CategoryRepository  { get; private set; }
+
+
+        public InternalUnit(ApplicationDbContext context)
+        {
+            _context = context;
+            CategoryRepository = new CategoryRepository(_context);
+        }
+        public void Save()
+        {
+           _context.SaveChanges();
+        }
+    }
+}
