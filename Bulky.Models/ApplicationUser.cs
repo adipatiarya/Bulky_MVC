ae2ab6254d6541e6910f463f426f9bc50ec16f3c
Kirun <you@example.com>
2025-10-18 16:22:44 +0700

    Register An Application User

---
+++ Bulky.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bulky.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Required]
+        public String Name { get; set; }
+        public String? StreetAddress { get; set; }
+        public String? City { get; set; }
+        public String? State { get; set; }
+        public String? PostalCode { get; set; }
+
+    }
+}
